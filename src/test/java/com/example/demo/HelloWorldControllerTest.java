63c9d06d29719b82513d0cfdfe077371ef36636d
shivammahajan19978 <https://shivammahajan19978@dev.azure.com>
2025-02-24 18:47:21 +0530

    initial commit

---
+++ src/test/java/com/example/demo/HelloWorldControllerTest.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest
+class HelloWorldControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void helloWorld_ShouldReturnHelloWorld() throws Exception {
+        mockMvc.perform(get("/api/v1/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello World"));
+    }
+    
+
+    @Test
+    void helloWorld_ShouldNotReturnHelloWorld() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isNotFound());
+    }
+}
