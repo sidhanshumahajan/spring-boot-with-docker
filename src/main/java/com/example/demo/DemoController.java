75c534ebcfce82466ce0f4bcabd369650edc0244
shivammahajan19978 <https://shivammahajan19978@dev.azure.com>
2025-02-25 17:07:21 +0530

    refactor code

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,15 +1,14 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1")
+//@RequestMapping("/api/v1")
 public class DemoController {
 
 	
-	@GetMapping("/hello")
+	@GetMapping("/")
 	public String helloWorld() {
 		return "Hello World";
 	}
