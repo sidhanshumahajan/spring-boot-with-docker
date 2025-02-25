365647e778b42b0bc4420f71eb24acd05aa80d9b
shivammahajan19978 <https://shivammahajan19978@dev.azure.com>
2025-02-25 19:59:08 +0530

    refactor code

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 //@RequestMapping("/api/v1")
 public class DemoController {
 
-	
 	@GetMapping("/")
 	public String helloWorld() {
 		return "Hello World";
