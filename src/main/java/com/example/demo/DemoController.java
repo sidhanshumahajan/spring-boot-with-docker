0c49cdb9c268ffac8e11cbe74a7cba65f5820f4b
sidhanshumahajan <47296472+sidhanshumahajan@users.noreply.github.com>
2025-03-01 17:03:30 +0530

    Update DemoController.java

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 //@RequestMapping("/api/v1")
 public class DemoController {
 
-	private static final String VALUE = "hello";
+	// private static final String VALUE = "hello";
 
 	@GetMapping("/")
 	public String helloWorld() {
